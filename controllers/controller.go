bcd4d831ecd2a1b1e357f97e698f78ee3842289c
Alisson <alisson.paulino@aluno.unifapce.edu.br>
2023-11-27 15:09:17 -0300

    BasicFront + CompleteBack de insert de produto

--- controllers/controller.go
+++ controllers/controller.go
@@ -3,13 +3,17 @@ package controllers
 import (
 	"database/sql"
 	"fmt"
-	"net/http"
-	"time"
 	"html/template"
 	"io/ioutil"
-	// "log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"log"
 
 	"goapp/models"
+
+	// "vendor/golang.org/x/crypto/chacha20"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -23,11 +27,7 @@ func CadastrarUser(w http.ResponseWriter, r *http.Request) {
 	// Validando se o método está como post
 
 	if r.Method == "POST" {
-		// if r.Method != http.MethodPost {
-		// 	http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
-		// 	return
-		// }
-		// Validando se o Mysql está de boa pra receber conexão
+
 		err := waitForMySQL()
 		if err != nil {
 			http.Error(w, "Erro ao aguardar o MySQL iniciar", http.StatusInternalServerError)
@@ -87,7 +87,7 @@ func ValidarLogin(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Erro ao validar usuário no banco de dados", http.StatusInternalServerError)
 			return
 		}
-		http.Redirect(w, r, "cadastro.html", http.StatusSeeOther)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
 
@@ -101,46 +101,54 @@ func SalvarProduto(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-        err = r.ParseMultipartForm(10 << 20)
-        if err != nil {
-            http.Error(w, "Erro ao analisar o formulário multipart", http.StatusInternalServerError)
-            return
-        }
+		err = r.ParseMultipartForm(10 << 20)
+		if err != nil {
+			http.Error(w, "Erro ao analisar o formulário multipart", http.StatusInternalServerError)
+			return
+		}
 		nome_product := r.FormValue("Nome")
 		desc_product := r.FormValue("Descricao")
 		lance_product := r.FormValue("Lance")
 		tmp_expiracao := r.FormValue("TmpExpiracao")
-        file, _, err := r.FormFile("foto")
-        if err != nil {
-            http.Error(w, "Erro ao obter o arquivo do formulário", http.StatusInternalServerError)
-            return
-        }
+		file, _, err := r.FormFile("foto")
+		if err != nil {
+			http.Error(w, "Erro ao obter o arquivo do formulário", http.StatusInternalServerError)
+			return
+		}
 		// Atrasando o fechamento do arquivo apenas pra quando a função tiver retorno
-        defer file.Close()
-		
-		// Validando se tudo foi preenchido
-		if nome_product == "" || desc_product == "" || lance_product == "" || tmp_expiracao == "" {
+		defer file.Close()
+
+		// Convertendo o valor recebido do formulário para int
+		lance_productInt, err1 := strconv.Atoi(lance_product)
+		tmp_expiracaoInt, err2 := strconv.Atoi(tmp_expiracao)
+		checaErro(err1, err2)
+
+		foto, err := ioutil.ReadAll(file)
+		if err != nil {
+			http.Error(w, "Erro ao ler o conteúdo do arquivo", http.StatusInternalServerError)
+			return
+		}
+
+		if nome_product == "" || desc_product == "" || lance_product == "" || tmp_expiracao == "" || len(foto) == 0{
 			http.Error(w, "Por favor, preencha todos os campos", http.StatusBadRequest)
 			return
 		}
 
-        foto, err := ioutil.ReadAll(file)
-        if err != nil {
-            http.Error(w, "Erro ao ler o conteúdo do arquivo", http.StatusInternalServerError)
-            return
-        }
-
-        usuario := models.Usuario{
-            Foto: foto,
-        }
+		insert_product := models.InsProduto{
+			Nome: nome_product,
+			Descricao: desc_product,
+			LanceInicial: lance_productInt,
+			TmpExpiracao: tmp_expiracaoInt,
+			FotoProduct: foto,
+		}
 		// Método do models pra inserir os dados e tratar algum erro que tiver
-		err = usuario.EnviarFoto()
+		err = insert_product.SalvarProduto()
 		// checaErro(err)
 		if err != nil {
 			http.Error(w, "Erro ao enviar foto para o banco de dados", http.StatusInternalServerError)
 			return
 		}
-		http.Redirect(w, r, "/", http.StatusSeeOther)
+		http.Redirect(w, r, "listaProdutos.html", http.StatusSeeOther)
 	}
 }
 
@@ -177,8 +185,8 @@ func ListarProdutos(w http.ResponseWriter, r *http.Request) {
 	// 		{{range .}}
 	// 			<li>
 	// 				<img src="data:image/png;base64,{{.FotoProduct}}" alt="Foto do Produto">
-	// 				<strong>{{.Nome}}</strong> - 
-	// 				R$ {{.LanceInicial}} - 
+	// 				<strong>{{.Nome}}</strong> -
+	// 				R$ {{.LanceInicial}} -
 	// 				Expira em: {{.TmpExpiracao}} dias
 	// 			</li>
 	// 		{{end}}
@@ -186,17 +194,17 @@ func ListarProdutos(w http.ResponseWriter, r *http.Request) {
 	// </body>
 	// </html>
 	// `)
-	
-if err != nil {
-	http.Error(w, "Erro ao analisar o modelo HTML", http.StatusInternalServerError)
-	return
-}
 
-err = tmpl.Execute(w, produtos)
-if err != nil {
-	http.Error(w, "Erro ao executar o template HTML", http.StatusInternalServerError)
-	return
-}
+	if err != nil {
+		http.Error(w, "Erro ao analisar o modelo HTML", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, produtos)
+	if err != nil {
+		http.Error(w, "Erro ao executar o template HTML", http.StatusInternalServerError)
+		return
+	}
 
 }
 
@@ -225,8 +233,11 @@ func waitForMySQL() error {
 	}
 }
 
-// func checaErro(err error) {
-// 	if err != nil {
-// 		log.Fatal("Erro encontrado ao processar pedido: ", err)
-// 	}
-// }
+func checaErro(err1 error, err2 error) {
+	if err1 != nil {
+		log.Fatal("Erro encontrado ao processar pedido: ", err1)
+	}
+	if err2 != nil {
+		log.Fatal("Erro encontrado ao processar pedido: ", err2)
+	}
+}
---
+++ models/insertproduct.go
@@ -0,0 +1,45 @@
+package models
+
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type InsProduto struct {
+	Nome         string 	`json:"nome"`
+	Descricao	 string 	`json:"descricao"`
+	LanceInicial int 		`json:"lance_inicial"`
+	TmpExpiracao int 		`json:"tmp_expiracao"`
+	FotoProduct  []byte 	`json:"Foto_Product"`
+}
+
+
+
+func ( i *InsProduto) SalvarProduto() error {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return fmt.Errorf("erro ao conectar ao banco de dados: %v", err)
+	}
+	defer db.Close()
+
+	result, err := db.Exec("insert into produtos(nome_product, desc_product, lance_inicial, tmp_expiracao, foto_product) VALUES (?, ?, ?, ?, ?)",
+		i. Nome, i.Descricao, i.LanceInicial, i.TmpExpiracao, i.FotoProduct)
+
+	if err != nil {
+		return fmt.Errorf("erro ao inserir foto no banco de dados: %v", err)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("erro ao obter número de linhas afetadas: %v", err)
+	}
+
+	if rowsAffected == 0 {
+		return fmt.Errorf("nenhuma linha foi afetada pela inserção")
+	}
+
+	return nil
+}
